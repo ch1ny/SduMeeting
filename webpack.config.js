25c36a21c717a823e05fd45e000bdd90b616501f
德布罗煜 <1056317718@qq.com>
2022-04-18 15:50:32 +0800

    添加flow:heavy_plus_sign:

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-react', '@babel/preset-env'],
+						presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-flow'],
 						plugins: ['@babel/plugin-proposal-class-properties'],
 					},
 				},
