3972ecaa3d5236954034e3ef6243bdf4a366fe67
德布罗煜 <1056317718@qq.com>
2022-04-18 15:57:29 +0800

    算了，现在加静态类型检查要改太多东西了:heavy_minus_sign:

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-flow'],
+						presets: ['@babel/preset-react', '@babel/preset-env'],
 						plugins: ['@babel/plugin-proposal-class-properties'],
 					},
 				},
