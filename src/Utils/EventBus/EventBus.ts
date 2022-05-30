c227cb9ffcfa755688b1cbfdc12d0c3e3aa5c25e
德布罗煜 <1056317718@qq.com>
2022-05-30 23:15:32 +0800

    为EventBUS添加invoke与handle函数:sparkles:

--- src/Utils/EventBus/EventBus.ts
+++ src/Utils/EventBus/EventBus.ts
@@ -1,93 +1,133 @@
 interface EventBusFunction {
-    func: Function,
-    once: boolean
+	func: Function;
+	once: boolean;
 }
 
 class EventBus {
-    events: {
-        [key: string]: EventBusFunction[]
-    };
+	events: {
+		[key: string]: EventBusFunction[];
+	};
+	handlers: {
+		[key: string]: Function;
+	};
 
-    constructor() {
-        this.events = {};
-    }
+	constructor() {
+		this.events = {};
+		this.handlers = {};
+	}
 
-    /**
-     * 为事件总线添加监听
-     * @param {string} type 添加的事件柄
-     * @param {Function} func 事件触发后执行的函数
-     */
-    on(type: string, func: Function) {
-        if (!this.events[type]) this.events[type] = [];
-        this.events[type].push({
-            func,
-            once: false,
-        });
-    }
+	/**
+	 * 为事件总线添加监听
+	 * @param {string} type 添加的事件柄
+	 * @param {Function} func 事件触发后执行的函数
+	 */
+	on(type: string, func: Function) {
+		if (!this.events[type]) this.events[type] = [];
+		this.events[type].push({
+			func,
+			once: false,
+		});
+	}
 
-    /**
-     * 为事件总线添加监听，只监听一次
-     * @param {string} type 添加的事件柄
-     * @param {Function} func 事件触发后执行的函数
-     */
-    once(type: string, func: Function) {
-        if (!this.events[type]) this.events[type] = [];
-        this.events[type].push({
-            func,
-            once: true,
-        });
-    }
+	/**
+	 * 为事件总线添加监听，只监听一次
+	 * @param {string} type 添加的事件柄
+	 * @param {Function} func 事件触发后执行的函数
+	 */
+	once(type: string, func: Function) {
+		if (!this.events[type]) this.events[type] = [];
+		this.events[type].push({
+			func,
+			once: true,
+		});
+	}
 
-    /**
-     * 触发事件
-     * @param {string} type 要触发的事件类型
-     * @param  {...any} args 传入的参数
-     */
-    emit(type: string, ...args: any[]) {
-        if (this.events[type]) {
-            const cbs = this.events[type];
-            const newCbs = new Array();
-            while (cbs.length > 0) {
-                const cb = cbs.pop() as EventBusFunction;
-                cb.func.apply(this, args);
-                if (!cb.once) newCbs.push(cb);
-            }
-            if (newCbs.length === 0) delete this.events[type];
-            else this.events[type] = newCbs;
-        }
-    }
+	/**
+	 * 触发事件
+	 * @param {string} type 要触发的事件类型
+	 * @param  {...any} args 传入的参数
+	 */
+	emit(type: string, ...args: any[]) {
+		if (this.events[type]) {
+			const cbs = this.events[type];
+			const newCbs = new Array();
+			while (cbs.length > 0) {
+				const cb = cbs.pop() as EventBusFunction;
+				cb.func.apply(this, args);
+				if (!cb.once) newCbs.push(cb);
+			}
+			if (newCbs.length === 0) delete this.events[type];
+			else this.events[type] = newCbs;
+		}
+	}
 
-    /**
-     * 从事件总线上移除某个函数的监听
-     * @param {string} type 从哪个事件柄上移除函数
-     * @param {Function} func 需要移除的函数
-     * @returns {boolean} 是否执行了移除事件
-     */
-    off(type: string, func: Function): boolean {
-        if (this.events && this.events[type]) {
-            const cbs = this.events[type];
-            let index = -1;
-            for (const cb of cbs) {
-                index++;
-                if (cb.func === func) {
-                    cbs.splice(index, 1);
-                    if (cbs.length === 0) delete this.events[type];
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	/**
+	 * 从事件总线上移除某个函数的监听
+	 * @param {string} type 从哪个事件柄上移除函数
+	 * @param {Function} func 需要移除的函数
+	 * @returns {boolean} 是否执行了移除事件
+	 */
+	off(type: string, func: Function): boolean {
+		if (this.events && this.events[type]) {
+			const cbs = this.events[type];
+			let index = -1;
+			for (const cb of cbs) {
+				index++;
+				if (cb.func === func) {
+					cbs.splice(index, 1);
+					if (cbs.length === 0) delete this.events[type];
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    /**
-     * 从事件总线上移除某个事件的全部监听
-     * @param {string} type 需要移除的事件柄
-     */
-    offAll(type: string) {
-        if (this.events) {
-            delete this.events[type];
-        }
-    }
+	/**
+	 * 从事件总线上移除某个事件的全部监听
+	 * @param {string} type 需要移除的事件柄
+	 */
+	offAll(type: string) {
+		if (this.events) {
+			delete this.events[type];
+		}
+	}
+
+	handle(type: string, cb: Function): { ok: boolean; err?: Error } {
+		if (this.handlers[type]) {
+			return {
+				ok: false,
+				err: new Error(`Handler '${type}' has already been registered.`),
+			};
+		} else {
+			this.handlers[type] = cb;
+			return {
+				ok: true,
+			};
+		}
+	}
+
+	invoke(type: string, ...args: any[]) {
+		const handler = this.handlers[type];
+		if (handler) {
+			return Promise.resolve(handler.apply(this, args));
+		} else {
+			throw Promise.resolve(new Error(`Handler '${type}' has not been registered yet.`));
+		}
+	}
+
+	invokeSync(type: string, ...args: any[]) {
+		const handler = this.handlers[type];
+		if (handler) {
+			return handler.apply(this, args);
+		} else {
+			throw new Error(`Handler '${type}' has not been registered yet.`);
+		}
+	}
+
+	removeHandler(type: string) {
+		delete this.handlers[type];
+	}
 }
 
 const eventBus = new EventBus();
